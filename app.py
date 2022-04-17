310232ee0333f66efee3d9707f6cd98d068b718a
anju-balan <77712155+anju-balan@users.noreply.github.com>
2022-04-17 19:13:47 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ def home():
     return render_template('homepage.html')
 
 @app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
 
 if __name__ == "__main__":
     app.run(debug=True)
