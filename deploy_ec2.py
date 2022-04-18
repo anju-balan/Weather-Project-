cfb778a6cd65ad06882fe96d1d04ed93c2f2002c
anjubalan992@gmail.com <anjubalan992@gmail.com>
2022-04-18 19:46:47 -0400

    updated path

--- deploy_ec2.py
+++ deploy_ec2.py
@@ -1,5 +1,6 @@
 import paramiko
 import sys
+import time 
 
 hostname = 'ec2-3-14-146-73.us-east-2.compute.amazonaws.com' 
 myuser   = 'ubuntu'
@@ -8,6 +9,7 @@ sshcon   = paramiko.SSHClient()  # will create the object
 sshcon.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # no known_hosts error
 sshcon.connect(hostname, username=myuser, key_filename=mySSHK) # no passwd needed
 stdin, stdout, stderr = sshcon.exec_command('/home/ubuntu/Weather-Project-/deploy.sh')
+time.sleep(10)
+sys.exit(0)
 print(stdout.readlines())
-sys.exit()
 sshcon.close()
