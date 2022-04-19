4b0638fff15d7215e0cb57085d8ba355866b020e
anjubalan992@gmail.com <anjubalan992@gmail.com>
2022-04-18 22:23:29 -0400

    updated .sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-rm /home/ubuntu/weather/Weather-Project-/nohup.out
-cd /home/ubuntu/weather/Weather-Project-/
+rm /home/ubuntu/weather/Weather_Project/nohup.out
+cd /home/ubuntu/weather/Weather_Project/
 git pull
-setsid nohup python3 /home/ubuntu/weather/Weather-Project-/app.py && exit
+setsid nohup python3 /home/ubuntu/weather/Weather_Project/app.py && exit
--- deploy_ec2.py
+++ deploy_ec2.py
@@ -9,7 +9,7 @@ sshcon   = paramiko.SSHClient()  # will create the object
 sshcon.set_missing_host_key_policy(paramiko.AutoAddPolicy()) # no known_hosts error
 sshcon.connect(hostname, username=myuser, key_filename=mySSHK) # no passwd needed
 
-stdin, stdout, stderr = sshcon.exec_command('/home/ubuntu/weather/Weather-Project-/deploy.sh')
+stdin, stdout, stderr = sshcon.exec_command('/home/ubuntu/weather/Weather_Project/deploy.sh')
 
 #stdin, stdout, stderr = sshcon.exec_command('/home/ubuntu/Weather-Project-/deploy.sh')
 time.sleep(10)
